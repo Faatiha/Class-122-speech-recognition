f6430c817c02959aba93e6641de40113f2a04b27
Faatiha <66500499+Faatiha@users.noreply.github.com>
2022-06-26 19:35:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+x = "";
+y = "";
+draw_circle = "";
+draw_rectangle = "";
+
+var speechRecognition = window.webkitSpeechRecognition;
+var recognition = new speechRecognition();
+
+function start(){
+    document.getElementById("status").innerHTML = "System is listening. Please speak";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized as: " + content;
+
+    if(content=="Circle"){
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+
+        document.getElementById("status").innerHTML = "Started drawing circle";
+
+        draw_circle = "set";
+    }
+    if(content=="rectangle"){
+        x = Math.floor(Math.random() * 800);
+        y = Math.floor(Math.random() * 500);
+
+        document.getElementById("status").innerHTML = "Started drawing rectangle";
+
+        draw_rectangle = "set";
+    }
+}
+
+function setup(){
+    canvas = createCanvas(900, 600);
+}
+
+function draw(){
+    if(draw_circle=="set"){
+     radius = Math.floor(Math.random() * 100);
+     circle(x,y,radius); 
+     document.getElementById("status").innerHTML = "circle is drawn";
+     draw_circle = "";  
+    }
+
+    if(draw_rectangle=="set"){
+        rect(x,y,70,50); 
+        document.getElementById("status").innerHTML = "rectangle is drawn";
+        draw_rectangle = "";  
+       }
+}
+
